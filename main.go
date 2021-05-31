274667e7378582b7996ceecac335a6f99c763a41
sgash708 <sugash7081023@gmail.com>
2021-05-31 11:13:18 +0900

    import要素の抽象化

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	elementCheck "github.com/sgash708/AlgoGoStudy/element"
+	element "github.com/sgash708/AlgoGoStudy/element"
 )
 
 func main() {
-	fmt.Println(elementCheck.RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2))
+	fmt.Println(element.RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2))
 }
