9ef2ba02e527a21cb9244f05a8265adf98c5a96f
sgash708 <sugash7081023@gmail.com>
2021-05-31 11:33:16 +0900

    不要コード削除

--- element/elementCheck.go
+++ element/elementCheck.go
@@ -1,20 +1,14 @@
 package elementcheck
 
-import (
-	"fmt"
-)
-
 // RemoveElement valと一致しない値に置き換える
 func RemoveElement(nums []int, val int) int {
 	i := 0
 	lengs := len(nums)
-	fmt.Println(nums)
 	for j := 0; j < lengs; j++ {
 		if nums[j] != val {
 			nums[i] = nums[j]
 			i++
 		}
 	}
-	fmt.Println(nums)
 	return i
 }
