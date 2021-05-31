3320ffe9478730253ad2e2730a00eee1ca12513d
sgash708 <sugash7081023@gmail.com>
2021-05-31 12:19:04 +0900

    TableDrivenに合わせて記述変更

--- element/elementCheck_test.go
+++ element/elementCheck_test.go
@@ -7,30 +7,47 @@ import (
 // TestRemoveElement テスト関数
 // ref: https://qiita.com/saya713y/items/f7ee07e8f12ab85ed9bf
 func TestRemoveElement(t *testing.T) {
-	t.Run("len=7", func(t *testing.T) {
-		// 期待する値
-		exp := 5
+	// TableDriven(DDD)
+	// PHPUnitのdataProviderのように振る舞わせる
+	cases := []struct {
+		nums []int
+		val  int
+		exp  int
+	}{
+		{nums: []int{0, 1, 2, 2, 3, 0, 4, 2}, val: 2, exp: 5},
+		{nums: []int{3, 2, 2, 3}, val: 2, exp: 2},
+	}
 
-		if RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2) != exp {
-			// 下記だと処理は終了される
-			// t.Fail() || t.FailNow()
-			// t.Fatal("結果:", res, "期待:", exp) || t.Fatalf()
-
-			// 処理は継続(t.Errorf()も同じ)
+	for _, c := range cases {
+		if RemoveElement(c.nums, c.val) != c.exp {
 			t.Error("期待する値と一致しませんでした。")
-			t.Fail()
 		}
-	})
+	}
 
-	t.Run("len=4", func(t *testing.T) {
-		// 期待する値
-		exp := 2
+	// t.Run("len=7", func(t *testing.T) {
+	// 	// 期待する値
+	// 	exp := 5
 
-		if RemoveElement([]int{3, 2, 2, 3}, 3) != exp {
-			t.Error("期待する値と一致しませんでした。")
-			t.Fail()
-		}
-	})
+	// 	if RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2) != exp {
+	// 		// 下記だと処理は終了される
+	// 		// t.Fail() || t.FailNow()
+	// 		// t.Fatal("結果:", res, "期待:", exp) || t.Fatalf()
+
+	// 		// 処理は継続(t.Errorf()も同じ)
+	// 		t.Error("期待する値と一致しませんでした。")
+	// 		t.Fail()
+	// 	}
+	// })
+
+	// t.Run("len=4", func(t *testing.T) {
+	// 	// 期待する値
+	// 	exp := 2
+
+	// 	if RemoveElement([]int{3, 2, 2, 3}, 3) != exp {
+	// 		t.Error("期待する値と一致しませんでした。")
+	// 		t.Fail()
+	// 	}
+	// })
 
 	// ログ出力
 	t.Log("TestRemoveElementの確認終了")
