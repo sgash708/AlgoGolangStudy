fd685ba55116916450b61fff938784671721724d
sgash708 <sugash7081023@gmail.com>
2021-05-31 12:10:28 +0900

    テストのリファクタリング

--- element/elementCheck_test.go
+++ element/elementCheck_test.go
@@ -8,28 +8,26 @@ import (
 // ref: https://qiita.com/saya713y/items/f7ee07e8f12ab85ed9bf
 func TestRemoveElement(t *testing.T) {
 	t.Run("len=7", func(t *testing.T) {
-		res := RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)
 		// 期待する値
 		exp := 5
 
-		if res != exp {
+		if RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2) != exp {
 			// 下記だと処理は終了される
 			// t.Fail() || t.FailNow()
 			// t.Fatal("結果:", res, "期待:", exp) || t.Fatalf()
 
 			// 処理は継続(t.Errorf()も同じ)
-			t.Error("結果:", res, "期待:", exp)
+			t.Error("期待する値と一致しませんでした。")
 			t.Fail()
 		}
 	})
 
 	t.Run("len=4", func(t *testing.T) {
-		res := RemoveElement([]int{3, 2, 2, 3}, 3)
 		// 期待する値
 		exp := 2
 
-		if res != exp {
-			t.Error("結果:", res, "期待:", exp)
+		if RemoveElement([]int{3, 2, 2, 3}, 3) != exp {
+			t.Error("期待する値と一致しませんでした。")
 			t.Fail()
 		}
 	})
